db9ad67ebf0c04f8f69075d55f1fe9ea46cca9bd
triplebmediake <triplebmediake@gmail.com>
2026-08-27 06:46:48 +0300

    Update AppLogo.tsx

--- src/components/ui/AppLogo.tsx
+++ src/components/ui/AppLogo.tsx
@@ -13,7 +13,7 @@ interface AppLogoProps {
 }
 
 const AppLogo = memo(function AppLogo({
-  src = '/assets/images/app_logo.png',
+  src = '/logo.jpg',
   iconName = 'SparklesIcon',
   size = 64,
   className = '',
